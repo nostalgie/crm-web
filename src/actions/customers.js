c958e8bc1948ef98d507df19dc13cf8edcd75828
screamcha <scream1234540@gmail.com>
2019-02-03 18:55:12 +0300

    fix customers

--- src/actions/customers.js
+++ src/actions/customers.js
@@ -2,42 +2,42 @@ import {
   CUSTOMERS_REQUEST_START,
   CUSTOMERS_REQUEST_SUCCESS,
   CUSTOMERS_REQUEST_FAILURE
-} from "constants/redux";
-import api from "services/api";
+} from 'constants/redux'
+import api from 'services/api'
 
 export const customersRequestStart = () => ({
   type: CUSTOMERS_REQUEST_START
-});
+})
 
 export const customersRequestSuccess = customers => ({
   type: CUSTOMERS_REQUEST_SUCCESS,
   customers
-});
+})
 
 export const customersRequestFailure = message => ({
   type: CUSTOMERS_REQUEST_FAILURE,
   message
-});
+})
 
 export const getCustomers = () => {
   return async (dispatch, getState) => {
-    dispatch(customersRequestStart());
-    const { authToken } = getState().user;
-    let result;
+    dispatch(customersRequestStart())
+    const { authToken } = getState().user
+    let result
 
     try {
-      result = await api.getCustomers(authToken);
-      const { customers } = result.data;
-      dispatch(customersRequestSuccess(customers));
-      return;
+      result = await api.getCustomers(authToken)
+      const { customers } = result.data
+      dispatch(customersRequestSuccess(customers))
+      return
     } catch (error) {
       if (error.response) {
-        const { code } = error.response.data;
-        let message = `Кто-то пошел не так. Код ошибки: ${code}`;
-        dispatch(customersRequestFailure(message));
+        const { code } = error.response.data
+        let message = `Кто-то пошел не так. Код ошибки: ${code}`
+        dispatch(customersRequestFailure(message))
       } else {
-        console.log(error);
+        console.log(error)
       }
     }
-  };
-};
+  }
+}
--- src/services/api.js
+++ src/services/api.js
@@ -1,77 +1,77 @@
-import Request from "./Request";
-import { periods } from "constants/menus/filter";
+import Request from './Request'
+import { periods } from 'constants/menus/filter'
 
 class ApiService {
-  constructor() {
-    this.request = new Request();
+  constructor () {
+    this.request = new Request()
   }
 
-  login(username, password) {
-    const url = "/login";
-    const payload = { username, password };
-    return this.request.post(url, { payload });
+  login (username, password) {
+    const url = '/login'
+    const payload = { username, password }
+    return this.request.post(url, { payload })
   }
 
-  getTickets({ state, period, customer, startDate, endDate }, token) {
-    const url = "/tickets";
-    if (state === "awaiting") {
-      state = "awaiting review";
+  getTickets ({ state, period, customer, startDate, endDate }, token) {
+    const url = '/tickets'
+    if (state === 'awaiting') {
+      state = 'awaiting review'
     }
 
-    let params = { state, period };
-    if (typeof customer === "number") {
-      params.customer = customer;
+    let params = { state, period }
+    if (typeof customer === 'number') {
+      params.customer = customer
     }
 
     if (period === periods.CUSTOM) {
-      params.startDate = startDate;
-      params.endDate = endDate;
+      params.startDate = startDate
+      params.endDate = endDate
     }
 
-    return this.request.get(url, { params }, token);
+    return this.request.get(url, { params }, token)
   }
 
-  getCustomers(token) {
-    const url = "/customers";
+  getCustomers (token) {
+    const url = '/customers'
 
-    return this.request.get(url, token);
+    return this.request.get(url, {}, token)
   }
 
-  getTicketInfo(ticketId, token) {
-    const url = `/tickets/${ticketId}`;
+  getTicketInfo (ticketId, token) {
+    const url = `/tickets/${ticketId}`
 
-    return this.request.get(url, {}, token);
+    return this.request.get(url, {}, token)
   }
 
-  addUpdate(token, ticketId, message, executorId) {
-    const url = "tickets/update";
+  addUpdate (token, ticketId, message, executorId) {
+    const url = 'tickets/update'
     const payload = {
       ticketId,
       message,
       executorId
-    };
+    }
 
-    return this.request.put(url, { payload }, token);
+    return this.request.put(url, { payload }, token)
   }
 
-  completeTicket(token, ticketId) {
-    const url = "tickets/finish";
+  completeTicket (token, ticketId) {
+    const url = 'tickets/finish'
     const payload = {
       ticketId
-    };
+    }
 
-    return this.request.post(url, { payload }, token);
+    return this.request.post(url, { payload }, token)
   }
 
-  rateTicket(token, ticketId, rating) {
-    const url = "tickets/rate";
+  rateTicket (token, ticketId, rating) {
+    const url = 'tickets/rate'
     const payload = {
       ticketId,
       rating
-    };
+    }
 
-    return this.request.post(url, { payload }, token);
+    return this.request.post(url, { payload }, token)
   }
 }
 
-export default new ApiService();
+export default new ApiService()
